81e0910eb653d3afe8246cc64b619ea9e3179ba5
alejandro <droguelios@gmail.com>
2025-12-22 07:58:20 -0500

    se realizo boton de alerta

---
+++ js/java.js
@@ -0,0 +1,6 @@
+const boton = document.querySelector("#botton");
+
+boton.addEventListener("click", () => {
+    alert("botón presionado, déjame en paz ");
+    console.log("botón presionado, déjame en paz ");
+});
